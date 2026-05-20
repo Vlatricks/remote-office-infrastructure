31b45f3c0f70795049e43244bd0c33d55371cd22
Vlatricks <vlatricks@vk.com>
2026-05-20 20:12:58 +0000

    implement backup script, cron schedule and prometheus alert rules

---
+++ src/backup/backup_vpn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# backup directory
+BACKUP_DIR="/var/backups/vpn"
+DATA_TO_BACKUP="/etc/openvpn /home/yc-user/clients/keys"
+
+# file name with date
+FILENAME="vpn_backup_$(date +%Y-%m-%d_%H%M%S).tar.gz"
+
+# make tar
+sudo tar -czf "$BACKUP_DIR/$FILENAME" $DATA_TO_BACKUP 2>/dev/null
+
+# checking successfully
+if [ $? -eq 0 ]; then
+    # Make "success flag" — file, which time Prometheus check
+    touch "$BACKUP_DIR/backup_success.flag"
+    echo "Backup made successfully: $FILENAME"
+else
+    echo "Error making backup!"
+    exit 1
+fi
+
+# Delete > 14 days
+find "$BACKUP_DIR" -name "vpn_backup_*.tar.gz" -mtime +14 -exec rm {} \;
+
